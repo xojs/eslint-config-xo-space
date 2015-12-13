42919273e0cac24e648503cd82b0a0e3eec72de8
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-13 23:03:05 +0100

    fix tests

--- test/test.js
+++ test/test.js
@@ -1,12 +1,12 @@
-'use strict';
-var test = require('ava');
-var isPlainObj = require('is-plain-obj');
-var eslint = require('eslint');
-var tempWrite = require('temp-write');
-var fixture = '\'use strict\';\nvar x = true;\n\nif (x) {\n  console.log();\n}\n';
+import test from 'ava';
+import isPlainObj from 'is-plain-obj';
+import eslint from 'eslint';
+import tempWrite from 'temp-write';
+
+const fixture = '\'use strict\';\nconst x = true;\n\nif (x) {\n  console.log();\n}\n';
 
 function runEslint(str, conf) {
-	var linter = new eslint.CLIEngine({
+	const linter = new eslint.CLIEngine({
 		useEslintrc: false,
 		configFile: tempWrite.sync(JSON.stringify(conf))
 	});
@@ -14,50 +14,38 @@ function runEslint(str, conf) {
 	return linter.executeOnText(str).results[0].messages;
 }
 
-test('main', function (t) {
-	var conf = require('../');
-
+test('main', t => {
+	const conf = require('../');
 	t.true(isPlainObj(conf));
 	t.true(isPlainObj(conf.env));
 	t.true(isPlainObj(conf.rules));
 	t.is(runEslint(fixture, conf).length, 0);
-
-	t.end();
 });
 
-test('browser', function (t) {
-	var conf = require('../browser');
-
+test('browser', t => {
+	const conf = require('../browser');
 	t.true(isPlainObj(conf));
 	t.true(isPlainObj(conf.env));
 	t.true(isPlainObj(conf.rules));
 	t.is(runEslint(fixture, conf).length, 0);
-
-	t.end();
 });
 
-test('esnext', function (t) {
-	var conf = require('../esnext');
-
+test('esnext', t => {
+	const conf = require('../esnext');
 	t.true(isPlainObj(conf));
 	t.true(isPlainObj(conf.env));
 	t.true(isPlainObj(conf.rules));
 
-	var errors = runEslint('class Foo {}\n', conf);
+	const errors = runEslint('class Foo {}\n', conf);
 	t.is(errors[0].ruleId, 'no-unused-vars');
-
-	t.end();
 });
 
-test('esnext es2016', function (t) {
-	var conf = require('../esnext');
-
+test('esnext es2016', t => {
+	const conf = require('../esnext');
 	t.true(isPlainObj(conf));
 	t.true(isPlainObj(conf.env));
 	t.true(isPlainObj(conf.rules));
 
-	var errors = runEslint('const x = {a: 0};\nasync function z() {\n\treturn Promise.resolve({b: 1, ...x});\n}\n', conf);
+	const errors = runEslint('const x = {a: 0};\nasync function z() {\n\treturn Promise.resolve({b: 1, ...x});\n}\n', conf);
 	t.is(errors[0].ruleId, 'no-unused-vars');
-
-	t.end();
 });
